859b15eb5f3e3c4bf198be5997dc29d3381182a7
lhty <yarik-87@mail.ru>
2021-05-12 17:02:25 +0300

    Add TS/Prettier/Eslint/pre-commit-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import cn from 'classnames';
 import s from './App.module.scss';
-import './styles/main.css';
 
 const App = () => {
   return <div className={cn(s.header, 'color')}>Initial</div>;
--- webpack.config.js
+++ webpack.config.js
@@ -14,18 +14,23 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
   },
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, './src/index.ts'),
   output: {
     path: path.resolve(__dirname, './dist/'),
     filename: 'main.js',
   },
   module: {
     rules: [
-      { test: /\.[tj]sx?$/, use: ['ts-loader'] },
+      { test: /\.[tj]sx?$/, exclude: /node_modules/, use: ['ts-loader'] },
       {
-        test: /\.(s*)css$/,
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
         use: [
           'style-loader',
+          'css-modules-typescript-loader?modules',
           {
             loader: 'css-loader',
             options: {
